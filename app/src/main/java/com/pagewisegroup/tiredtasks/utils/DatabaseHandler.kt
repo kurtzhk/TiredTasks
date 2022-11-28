ad0971244fe354523af56ccefc9937999dd871f4
kurtzhk <kurtzhk@wwu.edu>
2022-11-27 16:42:54 -0800

    messaging fragment locally merged & refactored to match medication activity

--- app/src/main/java/com/pagewisegroup/tiredtasks/utils/DatabaseHandler.kt
+++ app/src/main/java/com/pagewisegroup/tiredtasks/utils/DatabaseHandler.kt
@@ -6,6 +6,7 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import com.pagewisegroup.tiredtasks.models.MedicationModel
+import com.pagewisegroup.tiredtasks.models.MessagesModel
 
 class DatabaseHandler(
     context: Context?
@@ -20,17 +21,27 @@ class DatabaseHandler(
         "CREATE TABLE " + MED_TABLE + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
         MEDDOSE + " TEXT, " + MEDNAME + " TEXT, " + MEDFREQ + " TEXT, " + MEDSTATUS + " INTEGER);"
 
+    //message template db stuff
+    private var MESTABLENAME = "messages"
+    private var MESSAGE = "message"
+
+    private var CREATE_MES_TABLE =
+        "CREATE TABLE " + MESTABLENAME + "(" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                MESSAGE + " TEXT);"
+
     private lateinit var db: SQLiteDatabase
 
     override fun onCreate(p0: SQLiteDatabase?) {
         if (p0 != null) {
             p0.execSQL(CREATE_MED_TABLE)
+            p0.execSQL(CREATE_MES_TABLE)
         }
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
         if (p0 != null) {
             p0.execSQL("DROP TABLE IF EXISTS $MED_TABLE")
+            p0.execSQL("DROP TABLE IF EXISTS $MESTABLENAME")
 
             onCreate(p0)
         }
@@ -83,5 +94,44 @@ class DatabaseHandler(
 
     fun deleteMedication(id: Int) {
         db.delete(MED_TABLE, "$ID=?", arrayOf(id.toString()))
+    }
+
+    //message template functions below
+    fun addTemplate(template: MessagesModel) {
+        val cv = ContentValues()
+
+        cv.put(MESSAGE, template.message)
+        db.insert(MESTABLENAME, null, cv)
+    }
+
+    fun deleteTemplate(id: Int) {
+        db.delete(MESTABLENAME, "$ID=?", arrayOf(id.toString()))
+    }
+
+    fun getTemplates(): ArrayList<MessagesModel> {
+        val templates = ArrayList<MessagesModel>()
+        val cur: Cursor
+
+        db.beginTransaction()
+
+        try {
+            cur = db.query(MESTABLENAME, null, null, null, null, null, null, null)
+
+            if (cur != null) {
+                if (cur.moveToFirst()) {
+                    do {
+                        var template = MessagesModel()
+                        template.id = cur.getInt(cur.getColumnIndex(ID))
+                        template.message = cur.getString(cur.getColumnIndex(MESSAGE))
+                        templates.add(template)
+                    } while (cur.moveToNext())
+                }
+                cur.close()
+            }
+        } finally {
+            db.endTransaction()
+        }
+
+        return templates
     }
 }
