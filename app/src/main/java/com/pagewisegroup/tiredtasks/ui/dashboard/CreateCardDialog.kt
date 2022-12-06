13c30b65049f311891a6d91ad015fa0600fcd416
Sierra Schuster <schusts3@wwu.edu>
2022-12-06 09:54:00 -0800

    last minor fixes

--- app/src/main/java/com/pagewisegroup/tiredtasks/ui/dashboard/CreateCardDialog.kt
+++ app/src/main/java/com/pagewisegroup/tiredtasks/ui/dashboard/CreateCardDialog.kt
@@ -2,6 +2,8 @@ package com.pagewisegroup.tiredtasks.ui.dashboard
 
 import android.content.DialogInterface
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +18,7 @@ class CreateCardDialog : DialogFragment(){
     lateinit var conditionEditText: EditText
     lateinit var symptomEditText: EditText
     lateinit var notesEditText: EditText
+    lateinit var saveButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +26,7 @@ class CreateCardDialog : DialogFragment(){
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.create_card_dialog, container, false)
-        val saveButton = view.findViewById<Button>(R.id.save_button)
+        saveButton = view.findViewById(R.id.save_button)
 
         val db = this.context?.let { CardDatabaseManager(it) }
         saveButton.setOnClickListener{
@@ -45,6 +48,16 @@ class CreateCardDialog : DialogFragment(){
         symptomEditText = view.findViewById<EditText>(R.id.symptom_input)
         notesEditText = view.findViewById<EditText>(R.id.notes_input)
 
+        saveButton.isEnabled = false
+        conditionEditText.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                saveButton.isEnabled = true
+            }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
     }
 
     override fun onDismiss(dialog: DialogInterface) {
