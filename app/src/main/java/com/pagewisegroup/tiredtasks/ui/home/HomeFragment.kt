f6c89dfa045fda6098aed6a44e235408739232b0
Sierra Schuster <schusts3@wwu.edu>
2022-12-06 00:31:19 -0800

    added homescreen functionality

--- app/src/main/java/com/pagewisegroup/tiredtasks/ui/home/HomeFragment.kt
+++ app/src/main/java/com/pagewisegroup/tiredtasks/ui/home/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.pagewisegroup.tiredtasks.ui.home
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -14,19 +17,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import com.pagewisegroup.tiredtasks.R
 import com.pagewisegroup.tiredtasks.databinding.FragmentHomeBinding
+import com.pagewisegroup.tiredtasks.ui.dashboard.CardDatabaseManager
 import com.pagewisegroup.tiredtasks.ui.dashboard.MedicationFragment
+import com.pagewisegroup.tiredtasks.ui.medication.DialogCloseListener
 import com.pagewisegroup.tiredtasks.ui.medication.MedicationActivity
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), DialogCloseListener {
 
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var todoAdapter: RecyclerView.Adapter<TodoRecyclerAdapter.ViewHolder>? = null
-    private var planAdapter: RecyclerView.Adapter<PlanRecyclerAdapter.ViewHolder>? = null
+
+    private lateinit var todoAdapter: TodoRecyclerAdapter
+    private lateinit var todoRecycler: RecyclerView
+    private var entries = 0
+    private lateinit var v: View
+    lateinit var db: TodoDatabaseManager
+    var tasks = mutableListOf<String>()
+    var priorities = mutableListOf<String>()
+    var energies = mutableListOf<String>()
 
     //manages the long press on the plan recycler item
     private val planItemTouchHelper by lazy {
@@ -47,7 +58,7 @@ class HomeFragment : Fragment() {
                 }
 
                 override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                    TODO("Not yet implemented")
+                    TODO("implement")
                 }
             }
         ItemTouchHelper(simpleItemTouchCallback)
@@ -72,7 +83,7 @@ class HomeFragment : Fragment() {
                 }
 
                 override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                    TODO("Not yet implemented")
+
                 }
             }
         ItemTouchHelper(simpleItemTouchCallback)
@@ -83,16 +94,34 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        return view
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        //add task button that opens dialog
+        root.findViewById<Button>(R.id.add_task_button).setOnClickListener{
+            val addTaskDialog = AddTaskDialog()
+            addTaskDialog.setTargetFragment(this, 0)
+            addTaskDialog.show(parentFragmentManager, "AddTaskDialog")
+        }
+
+        return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
-        val todoRecycler = view.findViewById<RecyclerView>(R.id.todo_recycler)
+
         val planRecycler = view.findViewById<RecyclerView>(R.id.plan_recycler)
         val medicationButton = view.findViewById<MaterialButton>(R.id.medication_button)
+        todoRecycler = view.findViewById(R.id.todo_recycler)
+        v = view
+        val tododb = TodoDatabaseManager(view.context)
+
+        //reading all the info from the db to display in recyclerview
+        updateView(tododb)
+        todoAdapter = TodoRecyclerAdapter(tasks, priorities, energies)
 
         medicationButton.setOnClickListener {
             val intent = Intent(activity, MedicationActivity::class.java)
@@ -104,20 +133,35 @@ class HomeFragment : Fragment() {
         //applying managers and adapters to the recyclerview
         todoRecycler.apply{
             layoutManager = LinearLayoutManager(activity)
-            adapter = TodoRecyclerAdapter()
-            planItemTouchHelper.attachToRecyclerView(this)
+            adapter = todoAdapter
+            todoItemTouchHelper.attachToRecyclerView(this)
         }
 
         //applying managers and adapters to the recyclerview
         planRecycler.apply{
             layoutManager = LinearLayoutManager(activity)
             adapter = PlanRecyclerAdapter()
-            todoItemTouchHelper.attachToRecyclerView(this)
+            planItemTouchHelper.attachToRecyclerView(this)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    //reads data from database
+    private fun updateView(db: TodoDatabaseManager){
+        tasks = db.readAllTasks()
+        priorities = db.readAllPriorities()
+        energies = db.readAllEnergy()
+    }
+
+    //reads new user entry from database and updates recyclerview
+    override fun handleDialogClose(dialogInterface: DialogInterface) {
+        db = TodoDatabaseManager(v.context)
+        updateView(db)
+        entries++
+        todoRecycler.adapter = TodoRecyclerAdapter(tasks, priorities, energies)
     }
 }
--- app/src/main/java/com/pagewisegroup/tiredtasks/ui/home/TodoRecyclerAdapter.kt
+++ app/src/main/java/com/pagewisegroup/tiredtasks/ui/home/TodoRecyclerAdapter.kt
@@ -3,36 +3,69 @@ package com.pagewisegroup.tiredtasks.ui.home
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.pagewisegroup.tiredtasks.R
+import org.w3c.dom.Text
 
-class TodoRecyclerAdapter : RecyclerView.Adapter<TodoRecyclerAdapter.ViewHolder>() {
+class TodoRecyclerAdapter(task: MutableList<String>, prior: MutableList<String>, ener: MutableList<String>) : RecyclerView.Adapter<TodoRecyclerAdapter.ViewHolder>() {
 
-    //test object array
-    private val text = arrayOf("one", "two", "three")
+    var tasks: MutableList<String> = task
+    var priorities: MutableList<String> = prior
+    var energies: MutableList<String> = ener
 
     //grabs the fields from the recycler list item
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        var itemText: TextView
+    inner class ViewHolder(itemView: View, task: MutableList<String>, prior: MutableList<String>, ener: MutableList<String>) : RecyclerView.ViewHolder(itemView){
+        var taskText: TextView
+        var priorityImage: ImageView
+
+        var tasks: MutableList<String>
+        var priorities: MutableList<String>
+        var energies: MutableList<String>
+
+        val db: TodoDatabaseManager
 
         init{
-            itemText = itemView.findViewById(R.id.todo_text)
+            taskText = itemView.findViewById(R.id.todo_text)
+            priorityImage = itemView.findViewById(R.id.todo_battery)
 
+            tasks = task
+            priorities = prior
+            energies = ener
+            db = TodoDatabaseManager(itemView.context)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.todo_list_item, parent, false)
-        return ViewHolder(v)
+        return ViewHolder(v, tasks, priorities, energies)
     }
 
     //populates the fields retrieved in ViewHolder class with data
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemText.text = text[position]
+        tasks = holder.tasks
+        priorities = holder.priorities
+        energies = holder.energies
+
+        holder.taskText.text = tasks[position]
+
+        if(energies[position] == "Low"){
+            holder.priorityImage.setImageResource(R.drawable.battery3)
+        }else if(energies[position] == "High"){
+            holder.priorityImage.setImageResource(R.drawable.battery)
+        }
+
+        if(priorities[position] == "Low"){
+            holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+        }else if(priorities[position] == "High"){
+            holder.itemView.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+        }
     }
 
     override fun getItemCount(): Int {
-        return text.size
+        return tasks.size
     }
 }
